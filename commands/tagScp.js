531f08a9786c972899929d936f61dd9bc6a6364f
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-06-30 21:51:48 +0200

    Gotowa wersja

--- commands/tagScp.js
+++ commands/tagScp.js
@@ -21,23 +21,3 @@ var tagScp = function(msg, args) {
 };
 
 module.exports = tagScp;
-
-//var tag = function(msg, args) {
-//	console.log(args);
-////  if (args.length == 2) {
-//	msg.channel.send(new Discord.MessageEmbed()
-//		.setColor('#21d92a')
-//		.setTitle(args[1])
-//		.setURL(`http://scp-wiki.net.pl/system:page-tags/tag/${args[1]}`));
-//	}
-//  else if (args.length > 2) {
-//	var list = "";
-//	for (var i = 1; i <= args.length-1; i++) {
-//		list += `[${args[i]}](http://scp-wiki.net.pl/system:page-tags/tag/${args[i]}) \n`;
-//	}
-//	msg.channel.send(new Discord.MessageEmbed()
-//	.setColor('#21d92a')
-//	.setTitle('Tagi')
-//	.setDescription(list));
-//	}
-//};
