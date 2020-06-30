da9a499c1c33796b28b987dfe0eef2fb34278930
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-06-30 21:38:51 +0200

    Update tagScp.js

--- commands/tagScp.js
+++ commands/tagScp.js
@@ -1,24 +1,12 @@
 const Discord 	= require('discord.js');
 
-var help = function(msg, args) {
+var tagScp = function(msg, args) {
   console.log(args)
-  msg.channel.send(new Discord.MessageEmbed()
-  			    		.setColor('#21d92a')
-  			    		.setTitle('Lista komend')
-  			    		.setDescription('**• !pomoc** - wyświetla dostępne komendy \n' +
-  									'**• !scpfuel** - wysyła link do losowego obrazka zgodnego z licencją CC BY-SA 3.0 \n' +
-  									'**• !rand** `pl/ang` - wysyła link do losowego polskiego/angielskiego raportu \n' +
-  									'**• !wiki** `nazwa` - wysyła link do strony na polskiej wiki \n' +
-  									'**• !tag** `nazwa` - wysyła link do listy stron z odpowiednim tagiem \n' +
-  									'**• !scp** `numer` - wysyła link do angielskiego raportu. \n \n' +
-  									'   **Opcje wyszukiwania:** \n' +
-  									'**PL**`numer` - wyszukuje polski raport \n' +
-  									'**O**`numer` - wyszukuje raport na angielskiej wiki \n' +
-  									'`numer`**J** - wyszukuje raport Joke \n' +
-  									'`numer`**EX** - wyszukuje obiekty zrozumiane')
-  							);
+  if (args.length == 2) { console.log('Passed 1');}
+  else if (args.length > 2) {console.log('Passed 2');}
 };
-module.exports = help;
+
+module.exports = tagScp;
 
 //var tag = function(msg, args) {
 //	console.log(args);
