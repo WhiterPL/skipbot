78c891221f25c817e077f6c2b69929e726c4d3c8
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-06-30 21:32:04 +0200

    Update tagScp.js

--- commands/tagScp.js
+++ commands/tagScp.js
@@ -1,5 +1,25 @@
 const Discord 	= require('discord.js');
 
+var help = function(msg, args) {
+  console.log(args)
+  msg.channel.send(new Discord.MessageEmbed()
+  			    		.setColor('#21d92a')
+  			    		.setTitle('Lista komend')
+  			    		.setDescription('**• !pomoc** - wyświetla dostępne komendy \n' +
+  									'**• !scpfuel** - wysyła link do losowego obrazka zgodnego z licencją CC BY-SA 3.0 \n' +
+  									'**• !rand** `pl/ang` - wysyła link do losowego polskiego/angielskiego raportu \n' +
+  									'**• !wiki** `nazwa` - wysyła link do strony na polskiej wiki \n' +
+  									'**• !tag** `nazwa` - wysyła link do listy stron z odpowiednim tagiem \n' +
+  									'**• !scp** `numer` - wysyła link do angielskiego raportu. \n \n' +
+  									'   **Opcje wyszukiwania:** \n' +
+  									'**PL**`numer` - wyszukuje polski raport \n' +
+  									'**O**`numer` - wyszukuje raport na angielskiej wiki \n' +
+  									'`numer`**J** - wyszukuje raport Joke \n' +
+  									'`numer`**EX** - wyszukuje obiekty zrozumiane')
+  							);
+};
+module.exports = help;
+
 //var tag = function(msg, args) {
 //	console.log(args);
 ////  if (args.length == 2) {
@@ -19,25 +39,3 @@ const Discord 	= require('discord.js');
 //	.setDescription(list));
 //	}
 //};
-
-var tagScp = function(msg, args) {
-  console.log(args)
-  msg.channel.send(new Discord.MessageEmbed()
-  			    		.setColor('#21d92a')
-  			    		.setTitle('Lista komend')
-  			    		.setDescription('**• !pomoc** - wyświetla dostępne komendy \n' +
-  									'**• !scpfuel** - wysyła link do losowego obrazka zgodnego z licencją CC BY-SA 3.0 \n' +
-  									'**• !rand** `pl/ang` - wysyła link do losowego polskiego/angielskiego raportu \n' +
-  									'**• !wiki** `nazwa` - wysyła link do strony na polskiej wiki \n' +
-  									'**• !tag** `nazwa` - wysyła link do listy stron z odpowiednim tagiem \n' +
-  									'**• !scp** `numer` - wysyła link do angielskiego raportu. \n \n' +
-  									'   **Opcje wyszukiwania:** \n' +
-  									'**PL**`numer` - wyszukuje polski raport \n' +
-  									'**O**`numer` - wyszukuje raport na angielskiej wiki \n' +
-  									'`numer`**J** - wyszukuje raport Joke \n' +
-  									'`numer`**EX** - wyszukuje obiekty zrozumiane')
-  							);
-};
-
-
-exports.data = tagScp;
