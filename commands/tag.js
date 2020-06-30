59863cc9603da0b9a3c4efc4cbfed15fa2b4451d
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-06-30 09:58:19 +0200

    Update tag.js

--- commands/tag.js
+++ commands/tag.js
@@ -1,19 +1,22 @@
+const Discord 	= require('discord.js');
+
 var tag = function(msg, args[]) {
   if (args.length == 2) {
-					msg.channel.send(new Discord.MessageEmbed()
-                    	.setColor('#21d92a')
-                    	.setTitle(args[1])
-                    	.setURL(`http://scp-wiki.net.pl/system:page-tags/tag/${args[1]}`));
+	msg.channel.send(new Discord.MessageEmbed()
+		.setColor('#21d92a')
+		.setTitle(args[1])
+		.setURL(`http://scp-wiki.net.pl/system:page-tags/tag/${args[1]}`));
 	}
   else if (args.length > 2) {
-					var list = "";
-					for (var i = 1; i <= args.length-1; i++) {
-					  list += `[${args[i]}](http://scp-wiki.net.pl/system:page-tags/tag/${args[1]}) \n`;
-					}
-					msg.channel.send(new Discord.MessageEmbed()
-                    	.setColor('#21d92a')
-                    	.setTitle('Tagi')
-                    	.setDescription(list));
+	var list = "";
+	for (var i = 1; i <= args.length-1; i++) {
+		list += `[${args[i]}](http://scp-wiki.net.pl/system:page-tags/tag/${args[i]}) \n`;
+	}
+	msg.channel.send(new Discord.MessageEmbed()
+	.setColor('#21d92a')
+	.setTitle('Tagi')
+	.setDescription(list));
 	}
 };
+
 exports.data = tag;
