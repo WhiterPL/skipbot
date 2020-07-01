bcdf2046696a112012623818c1aa890a3f58fe68
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-07-01 20:47:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,27 +29,28 @@ client.on('message', msg => {
 	if (msg.author.bot || !msg.content.startsWith('!')){return;}
 	var args = msg.content.split(' ');
 	
-	switch(args[0].slice(1)) {
-		case 'pomoc':
+	switch(args[0]) {
+		case '!pomoc':
 			help(msg, args);
 		break;
-		case 'tag':
+		case '!tag':
 			tagScp(msg, args);
 		break;
-		case 'rand':
+		case '!rand':
 			randomScp(msg, args[1]);
 		break;
-		case 'scp':
+		case '!scp':
 			searchScp(msg, args);
 		break;
-		case 'wiki':
+		case '!wiki':
 			searchWiki(msg, args);
 		break;
-		case 'scpfuel':
+		case '!scpfuel':
 			fuelScp(msg, args);
 		break;
 		case 'r34':
 			msg.channel.send(new Discord.MessageAttachment('https://i.kym-cdn.com/entries/icons/original/000/033/758/Screen_Shot_2020-04-28_at_12.21.48_PM.png'));
+			console.log(`${msg.author.username} is horny!`);
 		break;
 	}
 });
