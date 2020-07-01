85b99da9929bf7a40ae04d6d0ec59e4f17918b8e
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-07-01 21:46:16 +0200

    Sprawdzanie ról

--- commands/postEmbed.js
+++ commands/postEmbed.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 var postEmbed = function(msg, args) {
-console.log(msg.member.roles);
+console.log(msg.member.roles.cache);
 };
 
 module.exports = postEmbed;
