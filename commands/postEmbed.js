4e96a9a7bdab27de98545b305341b2ea1a423ec7
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-07-01 22:11:38 +0200

    Update postEmbed.js

--- commands/postEmbed.js
+++ commands/postEmbed.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 var postEmbed = function(msg, args) {
   if(msg.member.roles.cache.some(role => role.name === 'OP') || msg.member.roles.cache.some(role => role.name === 'Half OP')) {
     console.log(args[1]);
-    const channel = args[1].slice(2,-2);
+    const channel = args[1].slice(2,-1);
     console.log(channel);
     channel.send("To działa");
     
