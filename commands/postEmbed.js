2fba0005f85137f3d0d34216f1b32f14da6f445d
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-07-01 21:58:03 +0200

    Test wysyłania na kanał

--- commands/postEmbed.js
+++ commands/postEmbed.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 var postEmbed = function(msg, args) {
   if(msg.member.roles.cache.some(role => role.name === 'OP') || msg.member.roles.cache.some(role => role.name === 'Half OP')) {
-    console.log(args[1]);
+    args[1].send("To działa");
   }
 };
 
