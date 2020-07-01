b2b200ebd367c35ae6c49d938f67d007c3f2329e
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-07-01 21:52:43 +0200

    Sprawdzanie ról

--- commands/postEmbed.js
+++ commands/postEmbed.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 var postEmbed = function(msg, args) {
-  if(msg.member.roles.cache.has('OP') || msg.member.roles.cache.has('Half OP')) {
+  if(msg.member.roles.cache.some(role => role.name === 'OP') || msg.member.roles.cache.some(role => role.name === 'Half OP')) {
     console.log("Masz OPa");
   }
 };
