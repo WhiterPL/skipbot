c6d5e7de606fd077de758be0414fbce62f2e279f
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-07-01 22:20:00 +0200

    Testowanie wysyłania na kanał

--- commands/postEmbed.js
+++ commands/postEmbed.js
@@ -3,9 +3,9 @@ const Discord = require('discord.js');
 var postEmbed = function(msg, args) {
   if(msg.member.roles.cache.some(role => role.name === 'OP') || msg.member.roles.cache.some(role => role.name === 'Half OP')) {
     console.log(args[1]);
-    const yourchannel = msg.guild.channels.find(channel => channel.id === args[1].slice(2,-1));
-    console.log(channel);
-    channel.send("To działa");
+    const yourchannel = msg.guild.channels.some(channel => channel.id === args[1].slice(2,-1));
+    console.log(yourchannel);
+    yourchannel.send("To działa");
     
   }
 };
