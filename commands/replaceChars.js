f8273b0d158799a4eacf620184d8afcb5e8ed8fe
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-07-01 19:05:49 +0200

    Create replaceChars.js

---
+++ commands/replaceChars.js
@@ -0,0 +1,6 @@
+var replaceChars = function replaceChars(a) {
+  a = a.replace(/a/g, 'a').replace(/ć/g, 'c').replace(/ż/g, 'z').replace(/ź/g, 'z').replace(/ł/g, 'l').replace(/ó/g, 'o').replace(/ś/g, 's').replace(/ę/g, 'e').replace(/ń/g, 'n');
+  return a;
+};
+
+module.exports = replaceChars;
