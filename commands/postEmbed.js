512feeaeef4ba5d967a902140cace02993022180
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-07-19 11:11:00 +0200

    Pobieranie atrybutów embeda

--- commands/postEmbed.js
+++ commands/postEmbed.js
@@ -1,11 +1,21 @@
 const Discord = require('discord.js');
 
 var postEmbed = function(msg, args) {
-  if(msg.member.roles.cache.some(role => role.name === 'OP') || msg.member.roles.cache.some(role => role.name === 'Half OP')) {
-    console.log(args[1]);
-    console.log(args[1].slice(1,-1));
+  if(msg.member.roles.cache.some(role => role.name === 'OP') || msg.member.roles.cache.some(role => role.name === 'Half OP') && args[1] != null) {
+  
+  for (var i = 3; i < args.length; i++) {
+    args[2] += ` ${args[i]}`;
+  }
+    
+  var embedAtribs = args[2].split(';');
+  console.log(embedAtribs);
+    
+  try {
     msg.client.channels.fetch(args[1].slice(2,-1))
-      .then(channel => channel.send(`Działa`));
+        .then(channel => channel.send(`Działa`));
+  } catch(e) {
+    msg.channel.send('Nieprawidłowy kanał'); 
+  }
   }
 };
 
