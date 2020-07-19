4a55b6275f1910611180c7b029d147d9856c4db8
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-07-19 10:54:07 +0200

    Test wysyłania na odpowiedni kanał

--- commands/postEmbed.js
+++ commands/postEmbed.js
@@ -3,10 +3,8 @@ const Discord = require('discord.js');
 var postEmbed = function(msg, args) {
   if(msg.member.roles.cache.some(role => role.name === 'OP') || msg.member.roles.cache.some(role => role.name === 'Half OP')) {
     console.log(args[1]);
-    const yourchannel = msg.guild.channels.find(channel => channel.id === args[1].slice(2,-1));
-    console.log(yourchannel);
-    yourchannel.send("To działa");
-    
+    console.log(args[1].slice(1,-1));
+    msg.client.channels.get(args[1].slice(1,-1)).send(`Działa`);
   }
 };
 
