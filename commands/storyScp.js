2c3df412fc593af48d3d60b83d1a78d099659703
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-09-20 21:00:16 +0200

    Test komendy hist

--- commands/storyScp.js
+++ commands/storyScp.js
@@ -2,11 +2,13 @@ const Discord = require('discord.js');
 const storyFile = require('./assets/story.json');
 
 var storyScp = function(msg, args) {
-  msg.channel.send(new Discord.MessageEmbed()
-  			    		.setColor('#21d92a')
-                .setTitle('Historia')
-                .setDescription(storyFile[0].content)
-                );
+  console.log('Funkcja działa');
+  console.log(storyFile);
+ // msg.channel.send(new Discord.MessageEmbed()
+ // 			    		.setColor('#21d92a')
+ //               .setTitle('Historia')
+ //               .setDescription(storyFile[0].content)
+ //               );
 };
 
 module.exports = storyScp;
