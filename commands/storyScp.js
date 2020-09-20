8e9a982fe35383a8af615a1f2e6e067967a907fd
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-09-20 21:05:18 +0200

    Update storyScp.js

--- commands/storyScp.js
+++ commands/storyScp.js
@@ -2,13 +2,11 @@ const Discord = require('discord.js');
 const storyFile = require('./assets/story.json');
 
 var storyScp = function(msg, args) {
-  console.log('Funkcja działa');
-  console.log(storyFile);
- // msg.channel.send(new Discord.MessageEmbed()
- // 			    		.setColor('#21d92a')
- //               .setTitle('Historia')
- //               .setDescription(storyFile[0].content)
- //               );
+  msg.channel.send(new Discord.MessageEmbed()
+  			    		.setColor('#21d92a')
+                .setTitle('Historia')
+                .setDescription(storyFile[0].content)
+                );
 };
 
 module.exports = storyScp;
