96503f31f22f8af410f9bd98037dc92dd810737b
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-09-21 23:37:16 +0200

    Dodawanie reakcji i przechodzenie dalej

--- commands/storyScp.js
+++ commands/storyScp.js
@@ -1,22 +1,46 @@
 const Discord = require('discord.js');
 const storyFile = require('./assets/story.json');
-const numToEmote = require('./numToEmote.js');
+
+var emotes = [
+    ":one:",
+    ":two:",
+    ":three:",
+    ":four:",
+    ":five:",
+    ":six:",
+    ":seven:",
+    ":eight:",
+    ":nine:",
+    ":zero:"
+  ];
 
 var storyScp = function(msg, args) {
   msg.channel.send(new Discord.MessageEmbed()
   			    		.setColor('#21d92a')
                 .setTitle('Historia')
-                .setDescription(storyFile[args[0]].content)
+                .setDescription(storyFile[args].content)
                 );
   
-  for (var i = 1; i <= storyFile[args[0]].goto.length; i++) {
-    msg.react(numToEmote(i));
-    
+  for (var i = 0; i < storyFile[args].goto.length; i++) {
+    msg.react(emotes[i]);
   }
+  
+  const filter = (reaction, user) => {
+	  return emotes.includes(reaction.emoji.name) && user.id === message.author.id;
+  };
+  
+  msg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+    for (var i = 0; i < 10; i++) {
+      if (reaction.emoji.name === emotes[i]) {
+        storyScp(msg, storyFile.goto[i]);
+      }
+    }
+	});
+  
   //TODO:
   // - Timestamp i autor
-  // - Bot czeka na reakcje
-  // - Reakcje przesyłają do następnych tekstów
 };
 
 module.exports = storyScp;
