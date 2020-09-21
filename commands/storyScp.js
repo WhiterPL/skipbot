a3b8a1103e6e7f39dde1d60a47e9a50db7f66ae1
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-09-21 23:14:00 +0200

    Dodawanie reakcji pod embedem

--- commands/storyScp.js
+++ commands/storyScp.js
@@ -1,12 +1,18 @@
 const Discord = require('discord.js');
 const storyFile = require('./assets/story.json');
+const numToEmote = require('./numToEmote.js');
 
 var storyScp = function(msg, args) {
   msg.channel.send(new Discord.MessageEmbed()
   			    		.setColor('#21d92a')
                 .setTitle('Historia')
-                .setDescription(storyFile[0].content)
+                .setDescription(storyFile[args[0]].content)
                 );
+  
+  for (var i = 1; i <= storyFile[args[0]].goto.length; i++) {
+    msg.react(numToEmote(i));
+    
+  }
   //TODO:
   // - Timestamp i autor
   // - Bot czeka na reakcje
