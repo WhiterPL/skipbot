cd23f2b4fcb5b841cf7f2a2c40a17696f9374fd0
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-10-19 11:05:45 +0200

    Przeniesienie operacji do promise

--- commands/storyScp.js
+++ commands/storyScp.js
@@ -15,30 +15,32 @@ const emotes = [
   ];
 
 var storyScp = function(msg, args) {
+	
   var story = new Discord.MessageEmbed()
   		.setColor('#21d92a')
                 .setTitle('Historia')
                 .setDescription(storyFile[args].content);
-  msg.channel.send(story);
-  
-  for (var i = 0; i < storyFile[args].goto.length; i++) {
-    story.react(emotes[i]);
-  }
-  
-  const filter = (reaction, user) => {
-	  return emotes.includes(reaction.emoji.name) && user.id === msg.author.id;
-  };
-  
-  msg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+	
+  msg.channel.send({embed: story}).then(embedMsg => {
+	  
+  	for (var i = 0; i < storyFile[args].goto.length; i++) {
+   		embedMsg.react(emotes[i]);
+ 	}
+	  
+	const filter = (reaction, user) => {
+		return emotes.includes(reaction.emoji.name) && user.id === msg.author.id;
+  	};
+	  
+	embedMsg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
 	.then(collected => {
 		const reaction = collected.first();
-    for (var i = 0; i < 10; i++) {
-      if (reaction.emoji.name === emotes[i]) {
-        storyScp(msg, storyFile.goto[i]);
-      }
-    }
+    		for (var i = 0; i < 10; i++) {
+      			if (reaction.emoji.name === emotes[i]) {
+        			storyScp(msg, storyFile.goto[i]);
+      			}
+    		}
 	});
-  
+  });
   //TODO:
   // - Timestamp i autor
 };
