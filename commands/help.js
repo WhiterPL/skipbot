8062ee82dc30584a19f3a29aca63d29124cc7485
WhiterPL <61293187+WhiterPL@users.noreply.github.com>
2020-12-02 10:39:23 +0100

    Dodano aliasy do pomocy

--- commands/help.js
+++ commands/help.js
@@ -6,12 +6,12 @@ var help = function(msg, args) {
   			    		.setTitle('Lista komend')
                 .setAuthor('Github', 'https://raw.githubusercontent.com/WhiterPL/skipbot/v1.1/commands/assets/github_white.png', 'https://github.com/WhiterPL/skipbot')
   			    		.setDescription('**• !pomoc** - wyświetla dostępne komendy \n' +
-  									'**• !scpfuel** - wysyła link do losowego obrazka zgodnego z licencją CC BY-SA 3.0 \n' +
-  									'**• !rand** `pl/ang` - wysyła link do losowego polskiego/angielskiego raportu \n' +
-  									'**• !wiki** `nazwa` - wysyła link do strony na polskiej wiki \n' +
+  									'**• !scpfuel** - wysyła link do losowego obrazka zgodnego z licencją CC BY-SA 3.0. Alias: **!sf**\n' +
+  									'**• !rand** `pl/ang` - wysyła link do losowego polskiego/angielskiego raportu. Alias: **!r** \n' +
+  									'**• !wiki** `nazwa` - wysyła link do strony na polskiej wiki. Alias: **!w**\n' +
                     '**• !hist** - pozwala zagrać w interaktywną grę tekstową \n' +            
-  									'**• !tag** `nazwa` - wysyła link do listy stron z odpowiednim tagiem \n' +
-  									'**• !scp** `numer` - wysyła link do angielskiego raportu. \n \n' +
+  									'**• !tag** `nazwa` - wysyła link do listy stron z odpowiednim tagiem. Alias: **!t**\n' +
+  									'**• !scp** `numer` - wysyła link do angielskiego raportu. Alias: **!s**\n \n' +
   									'   **Opcje wyszukiwania:** \n' +
   									'**PL**`numer` - wyszukuje polski raport \n' +
   									'**O**`numer` - wyszukuje raport na angielskiej wiki \n' +
