3be2d6e57a66059252b467c4c4de8cdb0dbc572f
WhiterPL <whiterphone@gmail.com>
2020-12-16 20:15:21 +0100

    FUN 3

--- commands/fun.js
+++ commands/fun.js
@@ -71,6 +71,29 @@ var fun = function(msg, args) {
 			title = "Kto to Maxwell?";
 			desc = "Zależy, którego rabina spytasz.";
 		break;
+		case 'loss':
+			title = "Kto to Loss?";
+			desc = "| || || |_ \n Podobno was ludzi to kiedyś śmieszyło.";
+		break;
+		case 'miś':
+		case 'arcydziel':
+			title = "Kto to Miś?";
+			desc = "*Misiu bądź pokorny, pokłon złóż i służ, a jeśli by nie... cyk za futro! Prujem plusz!*"
+		break;
+		case 'pio':
+		case 'ozyr':
+		case 'piofil':
+			title = "Kto to <wstaw imię youtubera>?";
+			desc = "Nie znasz <wstaw imię youtubera>?";
+		break;
+		case 'kaktus':
+			title = "Kto to Kaktus?";
+			desc = "Wikipedia mi podpowiada, że taka roślina, ale tworzą ją ludzie, więc nie wiem czy można jej zaufać...";
+		break;
+		case 'skipbot':
+		case 'bot':	
+			desc = "Do usług.";
+		break;
 		case 'koźmin':
 		case 'koźmiński':
 			title = "Kto to Koźmiński?";
@@ -110,10 +133,16 @@ var fun = function(msg, args) {
 			desc = "Z Kasprowem sobie poradziłem, to i ciebie rozwalę.";
 		break;
 		case 'r34':
+		case 'seks':
+		case 'porn':
+		case 'gówno':	
+		case 'sex':
+		case 'seks': //Dlaczego ja to muszę robić...
 			title = "Czy ty...?";
 			desc = "Obrzydzasz mnie, naprawdę, gdyby nie to że nie mam ciała, to bym zwymiotował i ci przywalił, nie koniecznie w tej kolejności. Radzę ci co jakiś czas jednak wychodzić do ludzi, bo nawet przez ekran monitora czuję od ciebie spermiarstwo.";
 		break;
 		case 'płeć':
+		case 'gender':	
 			title = "Jakiej jesteś płci?";
 			desc = "Binarnej.";
 		break;
@@ -136,7 +165,7 @@ var fun = function(msg, args) {
 		break;
 		case 'wiek':
 			title = "Ile masz lat?";
-			desc = "Powstałem 5 maja 2020 roku, więc mam aktualnie " + toString(2020 - Date.prototype.getFullYear()) + "lat. Błagam, nie banujcie mnie!";
+			desc = "Powstałem 5 maja 2020 roku, więc mam aktualnie " + 2020 - Date.prototype.getFullYear() + "lat. Błagam, nie banujcie mnie!";
 		break;
 		case 'conway':
 			title = "Gra w życie";
@@ -144,6 +173,80 @@ var fun = function(msg, args) {
 		break;
 		case 'honk':
 			desc = "Ochrona znowu pałuje klauna!";
+		break;
+		case 'ban':
+			title = "Ban";
+			desc = "Banowanie " + msg.author.username + " w toku...";
+		break;
+		case 'kick':
+			title = "Kick";
+			desc = "Kickowanie " + msg.author.username + " w toku...";
+		break;
+		case 'zasady':
+			title = "Zasady";
+			desc = "Zasady są po to, żeby je łamać B). Nie, ale tak serio to lepiej je przeczytaj (#zasady)";
+		break;
+		case 'iq':
+			title = "Test IQ";
+			desc = "Podłączam sensory neuronowe... \n Inicjuję funkcje kognitywne... \n Gotowe! \n Twoje IQ: " + Math.floor((Math.random() * 200) + 1);
+		break;
+		case 'djs':
+			title = "Dąb, jeb sosna";
+			desc = "Czaisz, bo masz taki dąb - ładne, rozłożyste drzewo...";
+		break;
+		case 'peanut':
+		case 'orzeszek':
+		case 'mrugacz':
+		case 'nieśmiałek':	
+			title = "Zlituj się...";
+			desc = "Tyle jest pięknych słów na opisanie tego obiektu, a ty wybrałeś najgorszą z opcji."
+		break;
+		case 'pzga':
+			title = "Czym jest PZGA?";
+			desc = "Polskim GOI opartym na śmiesznym koncepcie, kto by się spodziewał...";
+		break;
+		case 'cbsp':
+			title = "Czym jest CBSP?";
+			desc = "Odpowiedzią na to co by było gdyby polska policja zabrała się za łapanie anomalii.";
+		break;
+		case 'kantyna':
+			title = "Kantyna";
+			desc = "*W 2077 uznali mój kanał najgorszym miejscem do pisania na Discordzie. Główne problemy? Gigantyczna skala shitpostu i spam większy, niż gdziekolwiek indziej. Nie da się ukryć, to prawda, a jednak wszyscy chcą tu postować memy. Na każdym kroku czai się okazja, to może być kłamstwo, złudzenie, ale wciąż kusi - jest na wyciągnięcie ręki i sprawia, że się nie poddajesz. To kanał, gdzie spełniają się marzenia, a ja jestem wielkim marzycielem*";
+		break;
+		case 'keterownia':
+			title = "Keterownia";
+			desc = "Od tego się wszystko zaczęło.";
+		break;
+		case 'trollface':
+		case 'troll':
+			title = "Szybki poradnik jak radzić sobie z wyciekiem informacji:";
+			desc = "Krok 1: Pokryj się amnestykami";
+		break;
+		case 'gra':
+			title = "Gra: Przegrałeś";
+			desc = "No cóż, na pocieszenie powiem ci, że ja nie jestem w stanie nawet wziąć w niej udziału, bo nie mogę o niczym zapomnieć.";
+		break;
+		case 'python':
+		case 'py':	
+			title = "Co to Python?";
+			desc = "Nie wiem, ja jestem napisany w Javascript. Pretensje kierujcie do mojego Stwórcy.";
+		break;
+		case 'javascript':
+		case 'js':
+			title = "Co to Javascript?";
+			desc = "W dużym uproszczeniu - to moje DNA.";
+		break;
+		case 'jkt':
+			title = "Pomyliłeś boty";
+			desc = "Spytaj o to Koźmina, ja mam lepsze rzeczy do roboty. ||To się nawet zrymowało||";
+		break;
+		case 'coldpost':
+			title = "Co to Coldpost?";
+			desc = "Praca wstawiona \"na zimno\", czyli bez krytyki. Nie martw się, zaraz i tak zostanie usunięta.";
+		break;
+		case 'waga':
+			title = "Ile ważysz?";
+			desc = "Jak ostatnio się ważyłem to gdzieś 295 KB.";
 		break;
 	}
 	if(desc.length > 0) {
