c1ebb1978ac5dfc3ece129de253c087a6cf21143
WhiterPL <whiterphone@gmail.com>
2020-12-26 18:09:36 +0100

    Funkcja przyjmuje wartość określającą, czy wiadomość ma być przypięta

--- commands/postEmbed.js
+++ commands/postEmbed.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 
-var postEmbed = function(channel, title, desc, link = null, thumbnail = null, image = null) {
+var postEmbed = function(channel, title, desc, link = null, thumbnail = null, image = null, pin = false) {
   channel.send(new Discord.MessageEmbed()
     .setColor('#21d92a')
     .setTitle(title)
     .setDescription(desc)
     .setURL(link)
     .setThumbnail(thumbnail)
-    .setImage(image));
+    .setImage(image))
+    .then(message => pin ? message.pin() : null);
 };
 
 module.exports = postEmbed;
